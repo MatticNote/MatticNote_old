d843fe1d8848f69fa0290eb904c3d5fd79ec08ba
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-04-21 22:58:43 +0900

    API Route added (WIP)

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/YuzuRyo61/atreugo-csrf v0.0.0-20210421063428-f0e943cda1fe // indirect
+	github.com/atreugo/cors v1.0.4 // indirect
+	github.com/atreugo/websocket v1.0.7 // indirect
 	github.com/savsgio/atreugo/v11 v11.6.4 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
 )
--- server/route.go
+++ server/route.go
@@ -1,9 +1,29 @@
 package server
 
-import "github.com/savsgio/atreugo/v11"
+import (
+	"github.com/atreugo/cors"
+	"github.com/savsgio/atreugo/v11"
+)
 
 func ConfigureRoute(app *atreugo.Atreugo) {
 	app.GET("/", func(ctx *atreugo.RequestCtx) error {
+		// WIP
 		return ctx.TextResponse("GET")
+	})
+	apiPath := app.NewGroupPath("/api")
+
+	apiConfigureRoute(apiPath)
+}
+
+func apiConfigureRoute(p *atreugo.Router) {
+	p.UseAfter(cors.New(cors.Config{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS", "HEAD"},
+		AllowedHeaders:   []string{"Content-Type", "Accept", "Authorization", "Origin"},
+		AllowCredentials: false,
+	}))
+	p.GET("/", func(ctx *atreugo.RequestCtx) error {
+		// WIP
+		return ctx.TextResponse("API")
 	})
 }
